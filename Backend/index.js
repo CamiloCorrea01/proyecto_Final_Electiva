c01dcd19a74852c7a6858b754687b91371b3ee47
CamiloCorrea01 <38996951+CamiloCorrea01@users.noreply.github.com>
2024-12-16 08:18:46 -0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -25,17 +25,31 @@ app.use(express.json());
 app.use(cors());
 app.options('*', cors()); // Permitir preflight OPTIONS
 
-
 // Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+  })
+  .then(() => {
     console.log('Conectado a MongoDB');
-    generarAlertas();  // Ejecuta la función para generar alertas
-}).catch(err => {
+
+    // Generar alertas al iniciar la aplicación
+    generarAlertas();
+
+    // Programar la actualización de alertas cada 2 horas (7,200,000 ms)
+    setInterval(() => {
+      try {
+        console.log('Actualizando alertas...');
+        generarAlertas();
+      } catch (error) {
+        console.error('Error al actualizar alertas:', error);
+      }
+    }, 7200000); // 7,200,000 ms = 2 horas
+  })
+  .catch((err) => {
     console.error('Error de conexión:', err);
-});
+  });
 
 // Rutas principales
 app.use('/api/fondos', fondoRoutes);
@@ -43,12 +57,11 @@ app.use('/api/fondos/:fondoId/asignaciones', asignacionesRoutes);
 app.use('/api/alertas', alertaRoutes);
 app.use('/api/usuarios', userRoutes); // Agregar rutas de usuarios
 
-
 // Documentación Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 // Puerto del servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
